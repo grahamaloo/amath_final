8e6f732b07ddfbbb69ae71052e816276a1d5db4e
grahamaloo <graham.thomas.kelly@gmail.com>
2016-06-02 12:20:13 -0700

    cleanup

--- sig31.m
+++ sig31.m
@@ -6,9 +6,6 @@ function [SIG31] = sig31(flip, levels, N3, N1)
     for n = 1:N1
        ytemp = outputs(flip, levels);
        Ys(n,:) = ytemp;     
-%        y = outputs(flip, levels);
-%        x = zeros(N1, 1);
-%        x(n) = 1;  
     end
     for m = 1:N1
        SIG31 = SIG31 + Ys(:,m) * Xs(m,:); 
